dc571e63dc05b3aba320104c1cb81d16445d171e
brianchenoweth <brianmchenoweth@gmail.com>
2019-07-03 20:20:02 -0700

    Installing _s

---
+++ wp-content/themes/twentynineteen/front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
